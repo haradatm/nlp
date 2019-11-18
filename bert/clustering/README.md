dd939c9d8256d27e73dc6b332ac3e574853cedd0
Tomohiko HARADA <haradatm@haradatms-MacBook-Pro.local>
2019-11-19 08:50:18 +0900

    some updated

---
+++ bert/clustering/README.md
@@ -0,0 +1,156 @@
+# BERT-Embeddings Clustering (Chainer example code for Text Clustering using BERT embeddings)
+
+### Description
+
+This example code is a text clustering using BERT embeddings.
+
+### Dependencies
+- python 3.7
+- chainer 5.4
+
+In addition, please add the project folder to PYTHONPATH and `conca install` the following packages:
+- `matplotlib`
+
+### Usage ###
+
+### Preparation ###
+
+***BERT Pretrained model***
+
+  - Downlod [Pretrained model (English)](https://github.com/google-research/bert) and extract them in "BERT".
+  - Downlod [Pretrained model (Japanese)](http://nlp.ist.i.kyoto-u.ac.jp/DLcounter/lime.cgi?down=http://nlp.ist.i.kyoto-u.ac.jp/nl-resource/JapaneseBertPretrainedModel/Japanese_L-12_H-768_A-12_E-30_BPE.zip&name=Japanese_L-12_H-768_A-12_E-30_BPE.zip) and extract them in "BERT".
+
+***Data***
+
+  - [Scale Movie Review Dataset](https://www.cs.cornell.edu/people/pabo/movie-review-data/) (rt-polarity): Predict its sentiment (positive/negative) from a review about a movie.
+  - [Road Transport Bureau of MLIT](http://carinf.mlit.go.jp/jidosha/carinf/opn/index.html)
+  - Create train and test datasets and put them in the appropriate place.
+
+```
+wc -l datasets/rt-polarity/04-{train,test}.txt
+    9596 datasets/rt-polarity/04-train.txt
+    1066 datasets/rt-polarity/04-test.txt
+   10662 total
+
+head -n 3 datasets/rt-polarity/04-train.txt
+==> datasets/rt-polarity/04-train.txt <==
+0	simplistic , silly and tedious .
+0	it's so laddish and juvenile , only teenage boys could possibly find it funny .
+0	exploitative and largely devoid of the depth or sophistication that would make watching such a graphic treatment of the crimes bearable .
+
+head -n 3 datasets/rt-polarity/04-test.txt
+0	a visually flashy but narratively opaque and emotionally vapid exercise in style and mystification .
+0	while the performances are often engaging , this loose collection of largely improvised numbers would probably have worked better as a one-hour tv documentary .
+0	on a cutting room floor somewhere lies . . . footage that might have made no such thing a trenchant , ironic cultural satire instead of a frustrating misfire .
+```
+
+```
+wc -l datasets/mlit/04-{train,test}.txt
+   46743 datasets/mlit/04-train.txt
+    5197 datasets/mlit/04-test.txt
+   51940 total
+
+head -n 3 datasets/mlit/04-train.txt
+エンジン	車庫 に いれる ため に 、 右 後方 縦列 駐車 で バック して いた ところ 、 アクセル 操作 を して い ない のに 車 が 急 加速 し 、 右 後方 の 壁 に 激突 した 。
+エンジン	一般 道路 を 走行 中 、 突然 エンジン が 停止 した 。
+制動装置	高速 道路 を １００ ｋｍ／ｈ くらい で 走行 中 、 ＡＢＳ の マーク 、 サイド ブレーキ の マーク が 表示 さ れた 。
+
+head -n 3 datasets/mlit/04-test.txt
+車枠・車体	ダッシュボード が 溶けて ベトベト して いる 。
+排ｶﾞｽ･騒音	ＮＯＸ センサー の 不良に より 、 エンジン 警告 灯 が 点き っぱなし に なった 。
+車枠・車体	電動 オープン の ルーフ を 閉じる とき に 、 エラー メッセージ が 出て 幌 が 閉まら なく なった 。
+```
+
+***Run and Evaluate***
+
+- for rt-polarity datasets (for English)
+
+```
+python clustering_bert.py \
+--input  datasets/rt-polarity/04-train.txt \
+--vocab_file       BERT/uncased_L-12_H-768_A-12/vocab.txt \
+--bert_config_file BERT/uncased_L-12_H-768_A-12/bert_config.json \
+--init_checkpoint  BERT/uncased_L-12_H-768_A-12/arrays_bert_model.ckpt.npz \
+--gpu 0 \
+--batchsize 64 \
+--K 10 \
+--out results_bert-rt-all \
+2>&1 | tee train_bert-rt-all.log
+
+{
+  "gpu": 0,
+  "batchsize": 64,
+  "model": "models",
+  "input": "../datasets/rt-polarity/04-test.txt",
+  "init_checkpoint": "../../BERT/uncased_L-12_H-768_A-12/arrays_bert_model.ckpt.npz",
+  "bert_config_file": "../../BERT/uncased_L-12_H-768_A-12/bert_config.json",
+  "vocab_file": "../../BERT/uncased_L-12_H-768_A-12/vocab.txt",
+  "K": 10,
+  "max_length": null,
+  "out": "results_bert-rt-all",
+  "noplot": false
+}
+WARNING:tensorflow:From /content/drive/My Drive/Colab Notebooks/kenkyu/191118_clustering/bertlib/tokenization.py:74: The name tf.gfile.GFile is deprecated. Please use tf.io.gfile.GFile instead.
+
+2019-11-18 23:45:51,002 - load_data - INFO - Loading dataset ... done.
+# source: 1066, id: 2, {'0': 0, '1': 1}
+# vocab: 30522
+# K: 10
+# K: 10
+
+cluster_id	source_id
+0	0
+2	0
+2	0
+0	0
+6	0
+:
+```
+
+- for mlit datasets (for Japanese)
+
+```
+python clustering_bert.py \
+--input datasets/mlit/04-train.txt \
+--vocab_file       BERT/Japanese_L-12_H-768_A-12_E-30_BPE/vocab.txt \
+--bert_config_file BERT/Japanese_L-12_H-768_A-12_E-30_BPE/bert_config.json \
+--init_checkpoint  BERT/Japanese_L-12_H-768_A-12_E-30_BPE/arrays_bert_model.ckpt.npz \
+--gpu 0 \
+--batchsize 64 \
+--K 10 \
+--max_length 300 \
+--out results_bert-mlit-300 \
+2>&1 | tee train_bert-mlit-300.log
+
+{
+  "gpu": 0,
+  "batchsize": 64,
+  "model": "models",
+  "input": "datasets/mlit/04-train.txt",
+  "init_checkpoint": "BERT/Japanese_L-12_H-768_A-12_E-30_BPE/arrays_bert_model.ckpt.npz",
+  "bert_config_file": "BERT/Japanese_L-12_H-768_A-12_E-30_BPE/bert_config.json",
+  "vocab_file": "BERT/Japanese_L-12_H-768_A-12_E-30_BPE/vocab.txt",
+  "K": 10,
+  "max_length": 300,
+  "out": "results_bert-mlit-300",
+  "noplot": false
+}
+
+2019-11-18 23:42:03,405 - load_data - INFO - Loading dataset ... done.
+# source: 300, id: 15, {'車枠・車体': 0, '排ｶﾞｽ･騒音': 1, '制動装置': 2, 'エンジン': 3, '走行装置': 4, '乗車装置': 5, '燃料装置': 6, 'その他': 7, 'かじ取り': 8, '動力伝達': 9, '緩衝装置': 10, '保安灯火': 11, '電気装置': 12, '装置その他': 13, '電動機(モーター)': 14}
+# vocab: 32005
+# K: 10
+cluster_id	source_id
+3	0
+4	1
+6	0
+4	2
+2	3
+:
+```
+
+- Results (Scatter plot)
+
+|rt-polarity|mlit| 
+|---|---|
+![](results/results_bert-rt-all.png)|![](results/results_bert-mlit-300.png)
---
+++ bert/clustering/clustering_bert.py
@@ -0,0 +1,256 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Chainer example: Text clustering using a BoW encoder with BERT pre-trained embedding.
+
+"""
+
+__version__ = '0.0.1'
+
+import sys, time, logging, os, json, re, random
+import numpy as np
+np.set_printoptions(precision=20)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+console = logging.StreamHandler()
+console.setFormatter(logging.Formatter('%(asctime)s - %(funcName)s - %(levelname)s - %(message)s'))
+console.setLevel(logging.DEBUG)
+logger.addHandler(console)
+# logfile = logging.FileHandler(filename="log.txt")
+# logfile.setFormatter(logging.Formatter('%(asctime)s - %(funcName)s - %(levelname)s - %(message)s'))
+# logfile.setLevel(logging.DEBUG)
+# logger.addHandler(logfile)
+
+
+def pp(obj):
+    import pprint
+    pp = pprint.PrettyPrinter(indent=1, width=160)
+    logger.info(pp.pformat(obj))
+
+
+start_time = time.time()
+
+import chainer
+from chainer import cuda
+import chainer.functions as F
+import chainer.links as L
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import pickle
+from sklearn.metrics import confusion_matrix, classification_report
+
+
+from bertlib.modeling import BertConfig, BertModel
+from bertlib.tokenization import FullTokenizer
+
+
+class BertEmbedding(chainer.Chain):
+    def __init__(self, bert):
+        super(BertEmbedding, self).__init__()
+        with self.init_scope():
+            self.bert = bert
+
+    def __call__(self, x1, x2, x3):
+        output_layer = self.bert.get_pooled_output(x1, x2, x3)
+        return output_layer
+
+
+def load_data(path, tokenizer, bert_config, labels={}, max_length=None):
+    X, Y = [], []
+
+    for i, line in enumerate(open(path)):
+        if max_length is not None:
+            if i >= max_length:
+                break
+
+        line = line.strip()
+        line = line.replace(u'. . .', u'…')
+        if line == '':
+            continue
+
+        cols = line.split('\t')
+
+        if len(cols) > 1:
+            label, text = cols
+
+            if label not in labels:
+                labels[label] = len(labels)
+            Y.append(labels[label])
+        else:
+            text = cols[0]
+
+        text = text.strip()
+
+        tokens_a = tokenizer.tokenize(text)
+        tokens = ["[CLS]"]
+        segment_ids = [0]
+
+        for token in tokens_a:
+            tokens.append(token)
+            segment_ids.append(0)
+
+        input_ids = tokenizer.convert_tokens_to_ids(tokens)
+        input_mask = [1] * len(input_ids)
+
+        max_position_embeddings = bert_config.max_position_embeddings
+        x1 = np.array(input_ids[:max_position_embeddings], 'i')
+        x2 = np.array(input_mask[:max_position_embeddings], 'f')
+        x3 = np.array(segment_ids[:max_position_embeddings], 'i')
+        X.append((x1, x2, x3))
+
+    logger.info('Loading dataset ... done.')
+    sys.stdout.flush()
+
+    return X, Y, labels
+
+
+def batch_iter(data, batch_size):
+    batch = []
+
+    for line in data:
+        batch.append(line)
+        if len(batch) == batch_size:
+            yield tuple(list(x) for x in zip(*batch))
+            # yield batch
+            batch = []
+    if batch:
+        yield tuple(list(x) for x in zip(*batch))
+        # yield batch
+
+
+def to_device(device, x):
+    if device is None:
+        return x
+    elif device < 0:
+        return cuda.to_cpu(x)
+    else:
+        return cuda.to_gpu(x, device)
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(description='Chainer example: Clustering w/BERT')
+    parser.add_argument('--gpu', '-g', default=-1, type=int, help='GPU ID (negative value indicates CPU)')
+    parser.add_argument('--batchsize', '-b', default=64, type=int, help='learning batchsize size')
+    parser.add_argument('--model', default='models', type=str, help='model directory')
+    # parser.add_argument('--input', default='datasets/rt-polarity/04-train.txt', type=str, help='training file (.txt)')
+    # parser.add_argument('--init_checkpoint', default='BERT/uncased_L-12_H-768_A-12/arrays_bert_model.ckpt.npz', type=str, help='initial checkpoint (usually from a pre-trained BERT model (.npz)')
+    # parser.add_argument('--bert_config_file', default='BERT/uncased_L-12_H-768_A-12/bert_config.json', type=str, help='json file corresponding to the pre-trained BERT model (.json)')
+    # parser.add_argument('--vocab_file', default='BERT/uncased_L-12_H-768_A-12/vocab.txt', type=str, help='vocabulary file that the BERT model was trained on (.txt)')
+    parser.add_argument('--input', default='datasets/mlit/04-test.txt', type=str, help='training file (.txt)')
+    parser.add_argument('--init_checkpoint', default='BERT/Japanese_L-12_H-768_A-12_E-30_BPE/arrays_bert_model.ckpt.npz', type=str, help='initial checkpoint (usually from a pre-trained BERT model (.npz)')
+    parser.add_argument('--bert_config_file', default='BERT/Japanese_L-12_H-768_A-12_E-30_BPE/bert_config.json', type=str, help='json file corresponding to the pre-trained BERT model (.json)')
+    parser.add_argument('--vocab_file', default='BERT/Japanese_L-12_H-768_A-12_E-30_BPE/vocab.txt', type=str, help='vocabulary file that the BERT model was trained on (.txt)')
+    parser.add_argument('--K', '-K', default=None, type=int, help='number of cluster')
+    parser.add_argument('--max_length', default=None, type=int, help='maximum length of source data')
+    parser.add_argument('--out', '-o', default='results_bert-2', type=str, help='output prefix')
+    parser.add_argument('--noplot', action='store_true', help='disable PlotReport extension')
+    # parser.set_defaults(test=True)
+    args = parser.parse_args()
+    # args = parser.parse_args(args=[])
+    print(json.dumps(args.__dict__, indent=2))
+    sys.stdout.flush()
+
+    seed = 43
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+
+    if args.gpu >= 0:
+        chainer.backends.cuda.get_device_from_id(args.gpu).use()
+        cuda.check_cuda_available()
+        cuda.cupy.random.seed(seed)
+        chainer.config.use_cudnn = 'never'
+
+    # model_dir = args.model
+    # if not os.path.exists(model_dir):
+    #     os.mkdir(model_dir)
+
+    vocab_file = args.vocab_file
+    bert_config_file = args.bert_config_file
+    init_checkpoint = args.init_checkpoint
+
+    bert_config = BertConfig.from_json_file(bert_config_file)
+    tokenizer = FullTokenizer(vocab_file=vocab_file, do_lower_case=True)
+    label2id = {}
+
+    source_data, source_ids, label2id = load_data(args.input, tokenizer, bert_config, label2id, max_length=args.max_length)
+    id2label = {v: k for k, v in label2id.items()}
+
+    K = args.K if args.K is not None else len(label2id)
+    sys.stdout.flush()
+
+    print('# source: {}, id: {}, {}'.format(len(source_data), len(label2id), label2id))
+    print('# vocab: {}'.format(len(tokenizer.vocab)))
+    print('# K: {}'.format(K))
+    sys.stdout.flush()
+
+    # Setup model
+    bert = BertModel(config=bert_config)
+    model = BertEmbedding(bert)
+    chainer.serializers.load_npz(init_checkpoint, model, ignore_names=['output/W', 'output/b'])
+
+    if args.gpu >= 0:
+        model.to_gpu()
+
+    input_iter = batch_iter(source_data, args.batchsize)
+
+    with chainer.no_backprop_mode(), chainer.using_config('train', False):
+        outputs = []
+        for x1, x2, x3 in input_iter:
+            x1 = to_device(args.gpu, F.pad_sequence(x1, length=None, padding=0).array).astype('i')
+            x2 = to_device(args.gpu, F.pad_sequence(x2, length=None, padding=0).array).astype('f')
+            x3 = to_device(args.gpu, F.pad_sequence(x3, length=None, padding=0).array).astype('i')
+            y = model(x1, x2, x3)
+            outputs.append(cuda.to_cpu(y.data))
+
+    features = np.concatenate(outputs, axis=0)
+
+    from sklearn.cluster import KMeans
+    kmeans_model = KMeans(n_clusters=K, random_state=seed).fit(features)
+
+    if len(source_ids) > 0:
+        print("cluster_id\tsource_id")
+        for cluster_id, source_id in zip(kmeans_model.labels_, source_ids):
+            print("{}\t{}".format(cluster_id, source_id))
+    else:
+        print("cluster_id")
+        for cluster_id in kmeans_model.labels_:
+            print("{}".format(cluster_id))
+
+    if not args.noplot:
+        from sklearn.manifold import TSNE
+        tsne_model = TSNE(n_components=2, random_state=0).fit_transform(features)
+
+        if len(source_ids) > 0:
+            plt.figure(figsize=(20, 10))
+            plt.subplot(1, 2, 1)
+            plt.title("Input class")
+            for x, y, name in zip(tsne_model[:, 0], tsne_model[:, 1], [id2label[x] for x in source_ids]):
+            # for x, y, name in zip(tsne_model[:, 0], tsne_model[:, 1], source_ids):
+                plt.text(x, y, name, alpha=0.8, size=10)
+            plt.scatter(tsne_model[:, 0], tsne_model[:, 1], c=source_ids)
+            # plt.colorbar()
+
+            plt.subplot(1, 2, 2)
+            plt.title("K-means cluster")
+            for x, y, name in zip(tsne_model[:, 0], tsne_model[:, 1], [id2label[x] for x in source_ids]):
+            # for x, y, name in zip(tsne_model[:, 0], tsne_model[:, 1], source_ids):
+                plt.text(x, y, name, alpha=0.8, size=10)
+            plt.scatter(tsne_model[:, 0], tsne_model[:, 1], c=kmeans_model.labels_)
+            # plt.colorbar()
+
+        else:
+            plt.figure(figsize=(10, 10))
+            plt.title("K-means cluster")
+            plt.scatter(tsne_model[:, 0], tsne_model[:, 1], c=kmeans_model.labels_)
+            # plt.colorbar()
+
+        plt.savefig('{}.png'.format(args.out))
+        # plt.show()
+        plt.close()
+
+
+if __name__ == '__main__':
+    main()
+    logger.info('time spent: {:.6f} sec\n'.format(time.time() - start_time))
